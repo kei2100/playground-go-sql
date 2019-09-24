a41f244163b7ab631c002d19b50a9360b3fcf15d
kei2100 <kei.arima.2100@gmail.com>
2019-09-24 17:54:24 +0900

    sqlboiler

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/kei2100/playground-go-sql
 go 1.13
 
 require (
-	github.com/ArcanjoQueiroz/wait-for v0.0.3-0.20190921140424-b80612314665 // indirect
-	github.com/go-sql-driver/mysql v1.4.1 // indirect
-	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c // indirect
-	github.com/jessevdk/go-flags v1.4.0 // indirect
-	github.com/k0kubun/sqldef v0.5.3 // indirect
-	github.com/lib/pq v1.2.0 // indirect
-	google.golang.org/appengine v1.6.3 // indirect
+	github.com/DATA-DOG/go-sqlmock v1.3.3 // indirect
+	github.com/gofrs/uuid v3.2.0+incompatible // indirect
+	github.com/kat-co/vala v0.0.0-20170210184112-42e1d8b61f12
+	github.com/lib/pq v1.2.0
+	github.com/pkg/errors v0.8.1
+	github.com/spf13/viper v1.4.0
+	github.com/volatiletech/inflect v0.0.0-20170731032912-e7201282ae8d // indirect
+	github.com/volatiletech/null v8.0.0+incompatible // indirect
+	github.com/volatiletech/sqlboiler v3.5.0+incompatible
+	golang.org/x/sys v0.0.0-20190924062700-2aa67d56cdd7 // indirect
 )
--- sqlboiler-sqldef/postgres/schema.sql
+++ sqlboiler-sqldef/postgres/schema.sql
@@ -1,21 +1,21 @@
 CREATE TABLE public.accounts (
-    id char(36) NOT NULL PRIMARY KEY,
+    id varchar(36) NOT NULL PRIMARY KEY,
     account_id varchar(36) NOT NULL
 );
 CREATE UNIQUE INDEX accounts_idx_account_id on public.accounts (account_id);
 
 CREATE TABLE public.posts (
-    id char(36) NOT NULL PRIMARY KEY,
-    author_id char(36) NOT NULL,
+    id varchar(36) NOT NULL PRIMARY KEY,
+    author_id varchar(36) NOT NULL,
     body text NOT NULL,
     CONSTRAINT posts_fk_author_id FOREIGN KEY (author_id) REFERENCES public.accounts (id)
 );
 CREATE INDEX posts_idx_author_id on public.posts (author_id);
 
 CREATE TABLE public.comments (
-    id char(36) NOT NULL PRIMARY KEY,
-    post_id char(36) NOT NULL,
-    author_id char(36) NOT NULL,
+    id varchar(36) NOT NULL PRIMARY KEY,
+    post_id varchar(36) NOT NULL,
+    author_id varchar(36) NOT NULL,
     body text NOT NULL,
     CONSTRAINT comments_fk_author_id FOREIGN KEY (author_id) REFERENCES public.accounts (id),
     CONSTRAINT comments_fk_posts_id FOREIGN KEY (post_id) REFERENCES public.posts (id)
@@ -24,9 +24,9 @@ CREATE INDEX comments_idx_post_id on public.comments (post_id);
 CREATE INDEX comments_idx_author_id on public.comments (author_id);
 
 CREATE TABLE public.relationships (
-    id char(36) NOT NULL PRIMARY KEY,
-    follower_id char(36) NOT NULL,
-    followee_id char(36) NOT NULL,
+    id varchar(36) NOT NULL PRIMARY KEY,
+    follower_id varchar(36) NOT NULL,
+    followee_id varchar(36) NOT NULL,
     CONSTRAINT relationships_fk_follower_id FOREIGN KEY (follower_id) REFERENCES public.accounts (id),
     CONSTRAINT relationships_fk_followee_id FOREIGN KEY (followee_id) REFERENCES public.accounts (id)
 );
